09db0adf409bc36d724fe92e93ff3804a48a570f
lnmnd <lnmnd@users.noreply.github.com>
2015-05-11 23:30:18 +0200

    command type

--- src/Aco/Handler/AddArticleCollectionHandler.php
+++ src/Aco/Handler/AddArticleCollectionHandler.php
@@ -3,11 +3,16 @@
 namespace Aco\Handler;
 
 use Aco\Handler;
+use Aco\Command\AddArticleCollectionCommand;
 
 class AddArticleCollectionHandler implements Handler
 {
+	/**
+	 * @see \Aco\Handler::handle()
+	 * @param AddArticleCollectionCommand $command
+	 */
 	public function handle($command)
 	{
-		
+		echo "handle ".$command->title;
 	}
 }
