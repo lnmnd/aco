a7c005aa4c9f2f8b2caa9897f68936946ae28f95
lnmnd <lnmnd@users.noreply.github.com>
2015-05-19 12:13:51 +0200

    extract content: count paragraphs

--- src/Aco/Article.php
+++ src/Aco/Article.php
@@ -2,15 +2,22 @@
 
 namespace Aco;
 
+use Symfony\Component\DomCrawler\Crawler;
+use DOMDocument;
+use DOMElement;
+use Symfony\Component\DomCrawler\Symfony\Component\DomCrawler;
+
 class Article
 {
 	private $url;
 	private $originalContent;
+	private $content;
 	
 	public function __construct(UrlFetcher $urlFetcher, Url $url)
 	{
 		$this->url = $url;
 		$this->originalContent = $urlFetcher->fetch($url);
+		$this->content = $this->extractContent($this->originalContent);
 	}
 	
 	public function getUrl()
@@ -21,5 +28,61 @@ class Article
 	public function getOriginalContent()
 	{
 		return $this->originalContent;
+	}
+	
+	public function getContent()
+	{
+		return $this->content;
+	}
+	
+	private function extractContent($content)
+	{
+		$elements = $this->getAllElements($content);
+		$biggest_num = 0;
+    	$biggest_tag = null;
+    	foreach ($elements as $el) {
+            $p_num = $this->countParagraphs($el);
+            if ($p_num > $biggest_num) {
+        		$biggest_num = $p_num;
+        		$biggest_tag = $el;
+            }
+    	}
+
+    	if ($biggest_num == 0) {
+    		echo "todo no content";
+            return false;
+    	}
+		
+		return $this->innerHtml($biggest_tag);
+	}
+	
+	private function getAllElements($content)
+	{
+		$crawler = new Crawler($content);
+		return $crawler->filter('*');
+	}
+	
+	private function countParagraphs($node)
+	{
+		$n = 0;
+		foreach ($node->childNodes as $c) {
+			if (isset($c->tagName) && (($c->tagName == 'p') || ($c->tagName == 'P'))) {
+				$n++;
+			}
+		}
+		return $n;
+	}
+	
+	private function innerHtml(\DOMElement $element)
+	{
+		$innerHTML = "";
+		$children = $element->childNodes;
+		foreach ($children as $child) {
+			$tmp_dom = new DOMDocument();
+			$tmp_dom->appendChild($tmp_dom->importNode($child, true));
+			$innerHTML.=trim($tmp_dom->saveHTML());
+		}
+		
+		return $innerHTML;
 	}
 }
