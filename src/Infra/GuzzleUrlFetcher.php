0e1444d6bf3a201b170a435ca38c34fb64fc20c3
lnmnd <lnmnd@users.noreply.github.com>
2015-05-19 11:25:08 +0200

    catch guzzle exception

--- src/Infra/GuzzleUrlFetcher.php
+++ src/Infra/GuzzleUrlFetcher.php
@@ -3,16 +3,23 @@
 namespace Infra;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
 use Aco\UrlFetcher;
 use Aco\Url;
+use Aco\Exception\CannotFetchUrlException;
+use Aco\Exception\Aco\Exception;
 
 class GuzzleUrlFetcher implements UrlFetcher
 {
 	public function fetch(Url $url)
 	{
-		$client = new Client();
-		$response = $client->get($url->getUrl());
-		$body = (string)$response->getBody();
-		return $body;
+		try {
+			$client = new Client();
+			$response = $client->get($url->getUrl());
+			$body = (string)$response->getBody();
+			return $body;
+		} catch (ConnectException $e) {
+			throw new CannotFetchUrlException();
+		}
 	}
 } 
